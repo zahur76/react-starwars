cc06401a1d58f3cd8a9f0e1bfe46bf9e91d1a6e9
zahur <zahurmeerun@hotmail.com>
2021-11-26 07:22:12 +0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {  
+  render(){
+    return (
+      <h1 className="text-center">Starwars</h1>   
+           
+    );
+  }  
+}
+
+export default Header;
