4b0c46ba862a53c56e2f4f2337b21b7f9745625b
zahur <zahurmeerun@hotmail.com>
2021-11-26 18:05:17 +0400

    add welcome banner

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -4,8 +4,19 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import allCharactersData from '../data'
 
-class CharacterList extends React.Component {         
-    renderCharacters(){        
+class CharacterList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { display: 'block'}       
+        this.removeModal = this.removeModal.bind(this);        
+    }
+    removeModal(){
+        setTimeout(() => {
+            this.setState({display: 'none'})
+        }, 3000);
+    }          
+    renderCharacters(){
+        this.removeModal()        
         return (this.props.characters || allCharactersData).map(element=>{
             return  <Col className="character-details text-center" xs={12} sm={6} md={4} lg={3}>                     
                         <Col><img src={element.image} alt={element.image} /></Col>
@@ -24,7 +35,8 @@ class CharacterList extends React.Component {
             <div>                
                 <Row className="mt-3">
                     {this.renderCharacters()}
-                </Row>                
+                </Row>
+                <div className="banner text-center text-auto pt-5" style={{display: this.state.display}}>WELCOME !</div>                
             </div>                    
         );
     }  
